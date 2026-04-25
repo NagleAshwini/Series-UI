79ca9b6eccf8b0f1a66be54c3f58a0afc8dfdcef
NagleAshwini <162541081+NagleAshwini@users.noreply.github.com>
2026-04-25 17:15:19 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+// The new way for EPPlus 8 and later
+System.Environment.SetEnvironmentVariable("EPPlusLicenseContext", "NonCommercial"); var builder = WebApplication.CreateBuilder(args);
+// FORCE the license into the configuration manually
+builder.Configuration["EPPlus:ExcelPackage:LicenseContext"] = "NonCommercial";
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll",
+        builder => builder.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader());
+});
+
+var app = builder.Build();
+
+app.UseCors("AllowAll");
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
