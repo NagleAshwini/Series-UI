26d8fe4e66972d512e3c765260f3e69f7aa14cc3
NagleAshwini <162541081+NagleAshwini@users.noreply.github.com>
2026-05-08 12:32:58 +0530

    Add files via upload

---
+++ GetSeriesExcelQuery.cs
@@ -0,0 +1,11 @@
+﻿namespace SeriesUI.ExcelReport.Queries
+{
+    public class GetSeriesExcelQuery
+    {
+        public string SelectedYears { get; set; }
+        public GetSeriesExcelQuery(string years)
+        {
+            SelectedYears = years;
+        }
+    }
+}
